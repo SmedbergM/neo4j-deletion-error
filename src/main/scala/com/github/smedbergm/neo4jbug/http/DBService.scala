7aaa14b64e4d31c2783f5a1722f31b4b5daa6fe9
Matthew <msmedberg@lifetimefitness.com>
2017-03-02 17:27:06 -0700

    Add entirely non-random random foo generator

--- src/main/scala/com/github/smedbergm/neo4jbug/http/DBService.scala
+++ src/main/scala/com/github/smedbergm/neo4jbug/http/DBService.scala
@@ -1,12 +1,13 @@
 package com.github.smedbergm.neo4jbug.http
 
-import com.github.smedbergm.neo4jbug.neo4j.ExampleDB
-
+import com.github.smedbergm.neo4jbug.neo4j.{Bar, ExampleDB, Foo}
+import com.github.smedbergm.neo4jbug.utils.ScalazSupport
 import org.http4s._
 import org.http4s.dsl._
+
 import scalaz.concurrent.Task
 
-class DBService(db: ExampleDB) extends ResponseSupport {
+class DBService(db: ExampleDB) extends ResponseSupport with ScalazSupport {
 
   val handleRequest: PartialFunction[Request, Task[Response]] = {
     case GET -> Root => Ok(Usage.usage)
@@ -20,6 +21,16 @@ class DBService(db: ExampleDB) extends ResponseSupport {
       db.getBars(fooID).map{foos => foos.slice(start, end)}.flatMap(toTaskResponse).handleWith(toErrorResponse)
     case GET -> Root / "foo" / IntVar(fooID) / "bar" / IntVar(barID) =>
       db.getBar(barID, fooID).flatMap(toTaskResponse).handleWith(toErrorResponse)
+    case PUT -> Root / "foo" / IntVar(fooID) :? Name(name) =>
+      db.addFoo(Foo(fooID, name)).flatMap(toTaskResponse).handleWith(toErrorResponse)
+    case PUT -> Root / "foo" / IntVar(fooID) / "bar" / IntVar(barID) :? Name(name) =>
+      db.addBar(Bar(barID, name), fooID).flatMap(toTaskResponse).handleWith(toErrorResponse)
+    case PUT -> Root / "foo" / "random" :? N(optN) +& SingleTransaction(optSingleTransaction) => for {
+      n <- optN.orElse(Some(1000)).toTask
+      singleTransaction <- optSingleTransaction.orElse(Some(true)).toTask
+      foo <- db.generateFooWithBars(n, singleTransaction)
+      response <- toTaskResponse(foo).handleWith(toErrorResponse)
+    } yield response
   }
 
   val dbService = HttpService(handleRequest)
@@ -30,9 +41,15 @@ object Usage {
     """Available Routes:
       |GET /ping
       |GET /foo/{fooID}
-      |GET /foo/{fooID}/bar # returns an JSON array of all the Bars owned by that Foo
-      |GET /foo/{fooID}/bar/{barID}""".stripMargin
+      |GET /foo/{fooID}/bar
+      |GET /foo/{fooID}/bar?start={startIndex=0}&n={length=10} # returns an JSON array of the Bars owned by that Foo
+      |GET /foo/{fooID}/bar/{barID}
+      |PUT /foo/{fooID}?name={name}
+      |PUT /foo/{fooID}/bar/{barID}?name={name}
+      |PUT /foo/random # puts a foo with the next available fooID and gives it 1000 Bar children.""".stripMargin
 }
 
 object Start extends OptionalQueryParamDecoderMatcher[Int]("start")
 object N extends OptionalQueryParamDecoderMatcher[Int]("n")
+object Name extends QueryParamDecoderMatcher[String]("name")
+object SingleTransaction extends OptionalQueryParamDecoderMatcher[Boolean]("single-transaction")
--- src/main/scala/com/github/smedbergm/neo4jbug/http/ResponseSupport.scala
+++ src/main/scala/com/github/smedbergm/neo4jbug/http/ResponseSupport.scala
@@ -1,6 +1,6 @@
 package com.github.smedbergm.neo4jbug.http
 
-import com.github.smedbergm.neo4jbug.utils.{BadRequestException, NotFoundException}
+import com.github.smedbergm.neo4jbug.utils.{BadRequestException, EmptyOption, NonemptyOption, NotFoundException}
 import org.http4s.{MediaType, Response}
 import org.http4s.dsl._
 import org.json4s.ShortTypeHints
@@ -16,8 +16,13 @@ trait ResponseSupport {
   def toTaskResponse[T <: AnyRef](t: T): Task[Response] = Ok(write(t)).withType(MediaType.`application/json`)
 
   val toErrorResponse: PartialFunction[Throwable, Task[Response]] = {
-    case exc: BadRequestException => BadRequest(write(exc))
-    case exc: NotFoundException => NotFound(write(exc))
-    case exc => InternalServerError(s"Unexpected error: ${exc.toString}")
+    val makeResponse: PartialFunction[Throwable, Task[Response]] = {
+      case exc: BadRequestException => BadRequest(write(exc))
+      case NonemptyOption => BadRequest(write(NonemptyOption))
+      case exc: NotFoundException => NotFound(write(exc))
+      case EmptyOption => NotFound(write(EmptyOption))
+      case exc => InternalServerError(s"Unexpected error: ${exc.toString}")
+    }
+    makeResponse.andThen(response => response.withType(MediaType.`application/json`))
   }
 }
